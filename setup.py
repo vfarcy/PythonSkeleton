d51f859ac96ff9ce35b71571ecb5de35c3666116
vfarcy <vfarcy@free.fr>
2015-05-08 18:58:27 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+try:
+	from setuptools import setup
+
+except ImportError:
+	from distutils.core import setup
+	
+config = {
+	'description': 'My Project',
+	'author': 'My Name',
+	'url': 'URL to get it',
+	'download_url': 'Where to download it',
+	'author-email' : 'My email',
+	'
