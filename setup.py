2eab1866b5cae8d1772d92824820ee567e8f3b96
Valéry Farcy <vfarcy@free.fr>
2015-05-08 23:07:00 +0200

    change tab to 4 spaces

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 try:
-	from setuptools import setup
+    from setuptools import setup
 
 except ImportError:
-	from distutils.core import setup
-	
+    from distutils.core import setup
+
 config = {
-	'description': 'My Project',
-	'author': 'My Name',
-	'url': 'URL to get it',
-	'download_url': 'Where to download it',
-	'author-email' : 'My email',
-	'
+    'description': 'My Project',
+    'author': 'My Name',
+    'url': 'URL to get it',
+    'download_url': 'Where to download it',
+    'author-email' : 'My email',
+    '
